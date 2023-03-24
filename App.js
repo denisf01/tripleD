f7978d0e63a7fc5a174535ff7ec07c77ce660902
denisf01 <denis.folvarek@serapion.net>
2023-03-24 18:10:00 +0100

    Packages

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import axios from "axios";
 
 export default function App() {
+  const url =
+    "https://tripled-e3a40-default-rtdb.europe-west1.firebasedatabase.app/users.json";
+  const pressHandler = () => {
+    axios.post(url, {
+      firstName: 'Fred',
+      lastName: 'Flintstone'
+    })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title={"Click"} onPress={pressHandler} />
       <StatusBar style="auto" />
     </View>
   );
