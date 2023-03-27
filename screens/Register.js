51a667d69fd85dd6129b193d77f86731ac7cb4b1
denisf01 <denis.folvarek@serapion.net>
2023-03-27 19:52:24 +0200

    Added Address to register screen

--- screens/Register.js
+++ screens/Register.js
@@ -15,6 +15,7 @@ import {
   VStack,
   FormControl,
   Pressable,
+  ScrollView,
 } from "native-base";
 import { signUp_url } from "../constants/constants";
 import { useContext, useState } from "react";
@@ -32,12 +33,14 @@ const Register = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [rePassword, setRePassword] = useState("");
+  const [address, setAddress] = useState("");
   const [error, setError] = useState({
     userName: false,
     email: false,
     password: false,
     rePassword: false,
     fb: false,
+    address: false,
   });
   const ctx = useContext(Context);
 
@@ -97,6 +100,20 @@ const Register = (props) => {
         newState.rePassword = false;
         return newState;
       });
+      if (address.length <= 0) {
+        setError((prevState) => {
+          let newState = { ...prevState };
+          newState.address = true;
+          return newState;
+        });
+        return;
+      } else {
+        setError((prevState) => {
+          let newState = { ...prevState };
+          newState.address = false;
+          return newState;
+        });
+      }
     }
 
     axios
@@ -132,83 +149,92 @@ const Register = (props) => {
     <View style={styles.main}>
       <Center w="100%">
         <AppBar navigation={props.navigation} />
-        <Box safeArea p="2" w="90%" maxW="290" py="8">
-          <Heading
-            size="lg"
-            color="coolGray.800"
-            _dark={{
-              color: "warmGray.50",
-            }}
-            fontWeight="semibold"
-          >
-            Welcome
-          </Heading>
-          <Heading
-            mt="1"
-            color="coolGray.600"
-            _dark={{
-              color: "warmGray.200",
-            }}
-            fontWeight="medium"
-            size="xs"
-          >
-            Sign up to continue!
-          </Heading>
-          <VStack space={3} mt="5">
-            <FormControl>
-              <FormControl.Label>User Name</FormControl.Label>
-              <Input onChangeText={setUserName} />
-              {!!error && !!error.userName && (
-                <Text style={styles.error}>Enter a valid user name!</Text>
-              )}
-              <FormControl.Label>Email</FormControl.Label>
-              <Input onChangeText={setEmail} />
-              {!!error && !!error.email && (
-                <Text style={styles.error}>Enter a valid email!</Text>
-              )}
-            </FormControl>
-            <FormControl>
-              <FormControl.Label>Password</FormControl.Label>
-              <Input onChangeText={setPassword} type="password" />
-              {!!error && !!error.password && (
-                <Text style={styles.error}>
-                  Password must be at least 6 characters!
+        <ScrollView width={"100%"}>
+          <Box safeArea p="4" w="100%" maxW="100%" py="8">
+            <Heading
+              size="lg"
+              color="coolGray.800"
+              _dark={{
+                color: "warmGray.50",
+              }}
+              fontWeight="semibold"
+            >
+              Welcome
+            </Heading>
+            <Heading
+              mt="1"
+              color="coolGray.600"
+              _dark={{
+                color: "warmGray.200",
+              }}
+              fontWeight="medium"
+              size="xs"
+            >
+              Sign up to continue!
+            </Heading>
+            <VStack space={3} mt="5">
+              <FormControl>
+                <FormControl.Label>User Name</FormControl.Label>
+                <Input onChangeText={setUserName} />
+                {!!error && !!error.userName && (
+                  <Text style={styles.error}>Enter a valid user name!</Text>
+                )}
+                <FormControl.Label>Email</FormControl.Label>
+                <Input onChangeText={setEmail} />
+                {!!error && !!error.email && (
+                  <Text style={styles.error}>Enter a valid email!</Text>
+                )}
+              </FormControl>
+              <FormControl>
+                <FormControl.Label>Address</FormControl.Label>
+                <Input onChangeText={setAddress} />
+                {!!error && !!error.address && (
+                  <Text style={styles.error}>Enter a valid address!</Text>
+                )}
+              </FormControl>
+              <FormControl>
+                <FormControl.Label>Password</FormControl.Label>
+                <Input onChangeText={setPassword} type="password" />
+                {!!error && !!error.password && (
+                  <Text style={styles.error}>
+                    Password must be at least 6 characters!
+                  </Text>
+                )}
+              </FormControl>
+              <FormControl>
+                <FormControl.Label>Confirm Password</FormControl.Label>
+                <Input onChangeText={setRePassword} type="password" />
+                {!!error && !!error.rePassword && (
+                  <Text style={styles.error}>Password does not match!</Text>
+                )}
+                {!!error && !!error.fb && (
+                  <Text style={styles.error}>Email already exists!</Text>
+                )}
+              </FormControl>
+              <Button onPress={submitHandler} mt="2" colorScheme="indigo">
+                Sign up
+              </Button>
+              <HStack mt="6" justifyContent="center">
+                <Text
+                  fontSize="sm"
+                  color="coolGray.600"
+                  _dark={{
+                    color: "warmGray.200",
+                  }}
+                >
+                  Already have an account?{" "}
                 </Text>
-              )}
-            </FormControl>
-            <FormControl>
-              <FormControl.Label>Confirm Password</FormControl.Label>
-              <Input onChangeText={setRePassword} type="password" />
-              {!!error && !!error.rePassword && (
-                <Text style={styles.error}>Password does not match!</Text>
-              )}
-              {!!error && !!error.fb && (
-                <Text style={styles.error}>Email already exists!</Text>
-              )}
-            </FormControl>
-            <Button onPress={submitHandler} mt="2" colorScheme="indigo">
-              Sign up
-            </Button>
-            <HStack mt="6" justifyContent="center">
-              <Text
-                fontSize="sm"
-                color="coolGray.600"
-                _dark={{
-                  color: "warmGray.200",
-                }}
-              >
-                Already have an account?{" "}
-              </Text>
-              <Pressable
-                onPress={() => {
-                  props.navigation.navigate("login");
-                }}
-              >
-                <Text style={styles.text}>Sign In</Text>
-              </Pressable>
-            </HStack>
-          </VStack>
-        </Box>
+                <Pressable
+                  onPress={() => {
+                    props.navigation.navigate("login");
+                  }}
+                >
+                  <Text style={styles.text}>Sign In</Text>
+                </Pressable>
+              </HStack>
+            </VStack>
+          </Box>
+        </ScrollView>
       </Center>
     </View>
   );
